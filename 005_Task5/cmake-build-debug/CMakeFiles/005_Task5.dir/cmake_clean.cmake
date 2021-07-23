64e6c3eaa2d3181625cc9a8046a48f53938af48a
Alex <alex0692pash@mail.ru>
2021-07-23 22:59:23 +0300

    Init 005

---
+++ 005_Task5/cmake-build-debug/CMakeFiles/005_Task5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "005_Task5.exe"
+  "005_Task5.exe.manifest"
+  "005_Task5.pdb"
+  "CMakeFiles/005_Task5.dir/main.cpp.obj"
+  "lib005_Task5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/005_Task5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
